13386a9ed7dbca15ba0457abbd9173bfa9d83d97
Jujunol <jujunol@outlook.com>
2017-03-08 19:37:32 -0500

    Db connection added

---
+++ app/db/DB.ts
@@ -0,0 +1,24 @@
+import * as knex from 'knex';
+
+/**
+ * Created by Juju on 08/03/2017.
+ */
+let connection = null;
+export function DBConnection() {
+    if(!connection) {
+        connection = knex({
+            client: process.env.db_client,
+            connection: {
+                host: process.env.db_host,
+                database: process.env.db_name,
+                user: process.env.db_user,
+                password: process.env.db_password,
+            },
+        });
+    }
+    return connection;
+}
+
+export function DB(table: string): knex.QueryBuilder {
+    return DBConnection()(table);
+}
